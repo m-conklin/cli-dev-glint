520b65f353136c4a4805ea06a1af27ce19563758
Martin Conklin <mjmc@uvic.ca>
2015-03-24 20:43:37 +0000

    Add a template of the API to be filled out and a list of the parameter necessary to version controle.

---
+++ glintAPI.py
@@ -0,0 +1,42 @@
+# An API to commmunicate with jsonservices.py
+
+
+def getImages(USER_TOKEN, USER_TENANT){
+
+}
+
+def save(jsonMsg, USER_TOKEN, USER_TENANT){
+
+}
+
+def credentials(USER_TOKEN, USER_TENANT, USER_ID){
+
+}
+
+def listsiteS(USER_TOKEN, USER_TENANT){
+
+}
+
+def deletesite(USER_TOKEN, USER_TENANT, USER_ID, SITE_ID){
+
+}
+
+def createsite(USER_TOKEN, USER_TENANT, USER_ID, SITEDATA){
+
+}
+
+def deletecredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID){
+
+}
+
+def getcredential(USER_TOKEN, USER_TENANT,SITE_ID, USER_ID){
+
+}
+
+def hascredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID, CK_TYPE){
+
+}
+
+def addcredential(USER_TOKEN, USER_TENANT, CREDDATA){
+
+}
