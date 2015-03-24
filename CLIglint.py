fa97831b06c9a36840ce6c62a9fde23627df45ce
Martin Conklin <mjmc@uvic.ca>
2015-03-24 22:53:34 +0000

    Add CLI and a file for its logging and fix formatting in API shell

---
+++ CLIglint.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import glintAPI
+
+#print glintAPI.getImages("this", "works")
+
+
+
--- glintAPI.py
+++ glintAPI.py
@@ -1,42 +1,35 @@
 # An API to commmunicate with jsonservices.py
 
+#import logging
 
-def getImages(USER_TOKEN, USER_TENANT){
+#logging.basicConfig(filename='glintAPI.log', level=logging.DEBUG)
 
-}
+def getImages(USER_TOKEN, USER_TENANT):
+   return USER_TOKEN, USER_TENANT 
 
-def save(jsonMsg, USER_TOKEN, USER_TENANT){
+def save(jsonMsg, USER_TOKEN, USER_TENANT):
+    return jsonMsg, USER_TOKEN, USER_TENANT
 
-}
+def credentials(USER_TOKEN, USER_TENANT, USER_ID):
+    return USER_TOKEN, USER_TENANT, USER_ID
 
-def credentials(USER_TOKEN, USER_TENANT, USER_ID){
+def listsiteS(USER_TOKEN, USER_TENANT):
+    return USER_TOKEN, USER_TENANT
 
-}
+def deletesite(USER_TOKEN, USER_TENANT, USER_ID, SITE_ID):
+    return USER_TOKEN, USER_TENANT, USER_ID, SITE_ID
 
-def listsiteS(USER_TOKEN, USER_TENANT){
+def createsite(USER_TOKEN, USER_TENANT, USER_ID, SITEDATA):
+    return USER_TOKEN, USER_TENANT, USER_ID, SITEDATA
 
-}
+def deletecredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID):
+    return USER_TOKEN, USER_TENANT, SITE_ID, USER_ID
 
-def deletesite(USER_TOKEN, USER_TENANT, USER_ID, SITE_ID){
+def getcredential(USER_TOKEN, USER_TENANT,SITE_ID, USER_ID):
+    return USER_TOKEN, USER_TENANT,SITE_ID, USER_ID
 
-}
+def hascredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID, CK_TYPE):
+    return USER_TOKEN, USER_TENANT, SITE_ID, USER_ID, CK_TYPE
 
-def createsite(USER_TOKEN, USER_TENANT, USER_ID, SITEDATA){
-
-}
-
-def deletecredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID){
-
-}
-
-def getcredential(USER_TOKEN, USER_TENANT,SITE_ID, USER_ID){
-
-}
-
-def hascredential(USER_TOKEN, USER_TENANT, SITE_ID, USER_ID, CK_TYPE){
-
-}
-
-def addcredential(USER_TOKEN, USER_TENANT, CREDDATA){
-
-}
+def addcredential(USER_TOKEN, USER_TENANT, CREDDATA):
+    return USER_TOKEN, USER_TENANT, CREDDATA
