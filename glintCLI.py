9b5f71b4249a3ba4d7c339d0c130a43fae6c009e
Martin Conklin <mjmc@uvic.ca>
2015-03-27 17:37:25 +0000

    Remove areguments from inherited parser and add comments
    The variables USER_TOKEN, USER_TENANT, and USER_ID are now being handled by the API when it initializes

--- glintCLI.py
+++ glintCLI.py
@@ -5,7 +5,6 @@ import os
 import sys
 #import warnings
 
-#print glintAPI.getImages("this", "works")
 
 def env(*vars, **kwargs):
     """ Try to find the first environnental variable in vars,
@@ -26,6 +25,7 @@ class glintCommands(object):
         self.parser_class = parser_class
         self.api = glint_api('api.log', 'logging.DEBUG', 'api.yaml')
 
+    # create parser for arguments to be inherited by the subcommand subparsers
     def get_base_parser(self):
         self.parent = self.parser_class(
                 prog='glint',
@@ -36,65 +36,46 @@ class glintCommands(object):
 
         # Global arguments
 
-#        self.parent.add_argument('--user-token',
-#                             default=env('USER_TOKEN'),
-#                             help='Token used for authentication with the '
-#                                  'OpenStack Identity service. '
-#                                  'Defaults to env[USER_TOKEN].')
-        
-#        self.parent.add_argument('--user-tenant',
-#                             default=env('USER_TENANT'),
-#                             help='Tenant used for authentication with the '
-#                                  'OpenStack Identity service. '
-#                                  'Defaults to env[USER_TENANT].')
-
-
+        # used by save
         self.parent.add_argument('--json-message',
                              default=env('JSON_MESSAGE'),
                              help='Message used for authentication with the '
                                   'OpenStack Identity service. '
                                   'Defaults to env[JSON_MESSAGE].')
 
-#        self.parent.add_argument('--user-id',
-#                             default=env('USER_ID'),
-#                             help='User ID used for authentication with the '
-#                                  'OpenStack Identity service. '
-#                                  'Defaults to env[USER_ID].')
-
+        # used by delete-site, delete-credential, get-credential, and has-credential
         self.parent.add_argument('--site-id',
                              default=env('SITE_ID'),
                              help='Site ID used for authentication with the '
                                   'OpenStack Identity service. '
                                   'Defaults to env[SITE_ID].')
 
-
+        # used by create-site
         self.parent.add_argument('--site-data',
                              default=env('SITEDATA'),
                              help='Site data used for authentication with the '
                                   'OpenStack Identity service. '
                                   'Defaults to env[SITEDATA].')
 
-
+        # used by has-credential
         self.parent.add_argument('--ck-type',
                              default=env('CK_TYPE'),
                              help='CK type used for authentication with the '
                                   'OpenStack Identity service. '
                                   'Defaults to env[CK_TYPE].')
 
-
+        # used-by add-credential
         self.parent.add_argument('--cred-data',
                              default=env('CREDDATA'),
                              help='Credential data used for authentication with the '
                                   'OpenStack Identity service. '
                                   'Defaults to env[CREDDATA].')
          
+    
+    # default funtions for subcommand parsers to make calls to the glint API
 
     def getImages(self, args):
-        images = self.api.getImages()
-        if images:
-            return images
-        else:
-            print "ERROR"
+        return self.api.getImages()
 
     def save(self, args):
         return self.api.save(args.json_message)
@@ -124,82 +105,88 @@ class glintCommands(object):
         return self.api.addCredential(args.cred_data)
 
 
-    def get_sub_command_parser(self):
 
+
+    def get_sub_command_parser(self):
+        # create a new parser
         parser = self.parser_class(
                 prog='glint',
                 epilog='See "glint help COMMAND" '
                         'for help on a specific command.',
                 add_help=False
         )
-
+        
+        # create a subparser instance for the new parser
         subparser = parser.add_subparsers(prog='glint')
+        
 
-        # subparser for the get-images command
+        # subparsers to handle subcommands and inherit argument parsing
+
+        # get-images
         parser_getImages = subparser.add_parser('get-images',
                                                 parents=[self.parent], 
                                                 help='get images help')
         parser_getImages.set_defaults(func=self.getImages)
 
         
-        # subparser for the save command
+        # save
         parser_save = subparser.add_parser('save',
                                            parents=[self.parent],
                                            help='save help')
         parser_save.set_defaults(func=self.save)
 
 
-        # subparser for the credentials command
-        parser_credential = subparser.add_parser('credentials',
+        # credentials
+        parser_credentials = subparser.add_parser('credentials',
                                                  parents=[self.parent],
                                                  help='credentials help')
-        parser_credential.set_defaults(func=self.credentials)
+        parser_credentials.set_defaults(func=self.credentials)
 
 
 
-        # subparser for the list-sites command
+        # list-sites 
         parser_listSites = subparser.add_parser('list-sites',
                                                  parents=[self.parent],
                                                  help='list sites help')
         parser_listSites.set_defaults(func=self.listSites)
 
 
-        # subparser for the delete-site command
+        # delete-site
         parser_deleteSite = subparser.add_parser('delete-site',
                                                  parents=[self.parent],
                                                  help='delete site help')
         parser_deleteSite.set_defaults(func=self.deleteSite)
 
 
-        # subparser for the create-site command
+        # create-site
         parser_createSite = subparser.add_parser('create-site',
                                                  parents=[self.parent],
                                                  help='create site help')
         parser_createSite.set_defaults(func=self.createSite)
 
 
-        # subparser for the delete-credential command
+        # delete-credential 
         parser_deleteCredential = subparser.add_parser('delete-credential',
                                                  parents=[self.parent],
                                                  help='delete credential help')
         parser_deleteCredential.set_defaults(func=self.deleteCredential)
 
 
-        # subparser for the get-credential command
+        # get-credential 
         parser_getCredential = subparser.add_parser('get-credential',
                                                  parents=[self.parent],
                                                  help='get credential help')
         parser_getCredential.set_defaults(func=self.getCredential)
 
 
-        # subparser for the has-credential command
+        # has-credential
         parser_hasCredential = subparser.add_parser('has-credential',
                                                  parents=[self.parent],
                                                  help='has credential help')
         parser_hasCredential.set_defaults(func=self.hasCredential)
 
 
-        # subparser for the add-credential command
+        # add-credential
         parser_addCredential = subparser.add_parser('add-credential',
                                                  parents=[self.parent],
                                                  help='add credential help')
@@ -209,14 +196,14 @@ class glintCommands(object):
     
 
     def main(self, argv):
+        # initialize the parser to be inherited
         self.get_base_parser()
 
+        # setup subcommand parser and parse areguments
         subcommand_parser = self.get_sub_command_parser()
-
         command_args = subcommand_parser.parse_args(argv)
 
-#        print command_args.site_data
-
+        # call the handler function specified by the subcommand
         print command_args.func(command_args)
 
 
