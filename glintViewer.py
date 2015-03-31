4071d3c41a677a2fa76d079fb70c965ca44e92fc
Martin Conklin <mjmc@uvic.ca>
2015-03-31 23:48:52 +0000

    Add line and header formattign to prettyPrint

--- glintViewer.py
+++ glintViewer.py
@@ -3,3 +3,20 @@ import os, sys
 def prettyPrint(json, headers, colum_sizes):
 
 
+def line(colum_sizes):
+    rtn_line = '+'
+    for colum in colum_sizes:
+        rtn_line += '-'*(colum+2)
+        rtn_line += '+'
+    return rtn_line
+
+
+def header(headers, colum_sizes):
+    rtn_header = '| '
+    for header, colum in zip(headers, colum_sizes):
+        rtn_header += header
+        rtn_header += ' '*(colum-len(header)-1)
+        rtn_header += '| '
+    return rtn_header
+        
+    
