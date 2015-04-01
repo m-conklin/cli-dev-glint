dd30f29b438a600e2de31277a585401bead6167e
ronaldjosephdesmarais <ron.desmarais@gmail.com>
2015-04-01 11:26:54 -0700

    merged code

--- glintViewer.py
+++ glintViewer.py
@@ -1,5 +1,19 @@
 import os, sys
 
+import cli_factory_utils as fact_utils
+
+def cli_view(json,cmd):
+    print "View the Json %s with command %s"%(json,cmd)
+    #get titles
+    headers = fact_utils.get_titles(cmd)
+    #print headers
+    if "%s"%cmd in "get-images":
+        col_sizes = fact_utils.get_image_max_column_sizes(json,headers)
+    else:
+        col_sizes = fact_utils.get_max_column_sizes(json,headers)
+    print headers
+    print col_sizes
+
 def prettyPrint(json, headers, colum_sizes):
     print_line = line(colum_sizes)
     print print_line
