68937f7223333b2e716fda19b9f64b93a6d5f31f
Martin Conklin <mjmc@uvic.ca>
2015-04-08 18:23:38 +0000

    Add help functionality to gintCLI

--- api/glint_api.py
+++ api/glint_api.py
@@ -9,51 +9,44 @@ ok
 class glint_api(object):
     def __init__(self,log_name,log_lvl,glint_cfg):
         #setup logging for glint api
-        print "init glint_api"
+        print ""
         
     def getImages(self):
-        data_obj = {}
+        data_obj = {'images':'Image One'}
         return data_obj
 
     def imageDelete(self,image_name,img_src_site,image_src_tenent):
-        data_obj = {}
+        data_obj = {'image':'Deleted'}
         return data_obj
         
     def imageCopy(self,image_name,src_site,dest_sites):
-        data_obj = {}
+        data_obj = {'image':'Copied'}
         return data_obj
 
     def listSites(self):
-        data_obj = {}
+        data_obj = {'sites':'Site1'}
         return data_obj
 
     def deleteSite(self, SITE_ID):
-        data_obj = {}
+        data_obj = {'site':'Deleted'}
         return data_obj
 
     def createSite(self,name,url,formatt):
-        data_obj = {}
+        data_obj = {'site':'Created'}
         return data_obj
 
     def deleteCredential(self, site_id):
-        data_obj = {}
+        data_obj = {'credential':'Deleted'}
         return data_obj
 
     def getCredential(self,site_id):
-        data_obj = {}
+        data_obj = {'credetial':'Credentail1'}
         return data_obj
 
     def hasCredential(self, site_id, ck_type):
-        data_obj = {}
+        data_obj = {'credential':'Yes'}
         return data_obj
 
     def addCredential(self, remote_tenant,remote_un,remote_pw,remote_site_id):
-        data_obj = {}
+        data_obj = {'credential':'added'}
         return data_obj
-    
-    
-    
-    
-    
-    
-    
